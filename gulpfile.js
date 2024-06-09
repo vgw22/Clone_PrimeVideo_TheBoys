8151198eb48b01d97afc84dff71b73f66ff4cb82
vgw22 <vitorgabrielwinck2222@gmail.com>
2024-06-09 14:15:14 -0300

    adicionando arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const watch = require('gulp-watch');
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    gulp.src('./src/styles/*scss')
+    return gulp.src('./src/styles/*scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/styles'))
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(styles))
 }
